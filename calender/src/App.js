ca6bdaf10424373d74f2dd1aa91fe46177553121
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-03 16:09:39 +0530

    added the code of react in app.js and disabled index.css

--- calender/src/App.js
+++ calender/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import DateTimePicker from "react-datetime-picker";
 
 function App() {
+  const [reminderMsg, setReminderMsg] = useState("");
+  const [remindAt, setRemindAt] = useState();
+
+  const addReminder = () => {};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="homepage">
+        <div className="homepage_header">
+          <h1>Remind me 🙋🏻‍♂️</h1>
+          <input
+            type="text"
+            placeholder="Reminder notes here..."
+            value={reminderMsg}
+            onChange={(e) => setReminderMsg}
+          />
+          <DateTimePicker
+            value={remindAt}
+            onChange={setRemindAt}
+            minDate={new Date()}
+            minutePlaceholder="mm"
+            hourPlaceholder="hh"
+            dayPlaceholder="dd"
+            monthPlaceholder="mm"
+            yearPlaceholder="yyyy"
+          />
+          <div className="button" onClick={addReminder}>
+            Add Reminder
+          </div>
+        </div>
+        <div className="homepage_body">
+        
+          <div className="reminder_card">
+            <h2>Reminder notes</h2>
+            <h3>Remind me at:</h3>
+            <p> 26/05/2023 @2AM</p>
+            <div className="button">Delete</div>
+          </div>
+          <div className="reminder_card">
+            <h2>Reminder notes</h2>
+            <h3>Remind me at:</h3>
+            <p> 26/05/2023 @2AM</p>
+            <div className="button">Delete</div>
+          </div>
+          <div className="reminder_card">
+            <h2>Reminder notes</h2>
+            <h3>Remind me at:</h3>
+            <p> 26/05/2023 @2AM</p>
+            <div className="button">Delete</div>
+          </div>
+          <div className="reminder_card">
+            <h2>Reminder notes</h2>
+            <h3>Remind me at:</h3>
+            <p> 26/05/2023 @2AM</p>
+            <div className="button">Delete</div>
+          </div>
+
+
+        </div>
+      </div>
     </div>
   );
 }
